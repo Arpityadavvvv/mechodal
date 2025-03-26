2109b27ad8aaf2b4b62c2221faf2a7d8c75d7240
Arpityadavvvv <arpity665@gmail.com>
2025-03-26 21:16:38 +0530

    cors update

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // now using middlewares here 
 
 app.use(cors({
-    origin: "http://localhost:5173", // Allow frontend requests
+    origin: "https://mechodaltech.vercel.app", // Allow frontend requests
     methods: ["POST", "GET"], 
     credentials: true
   })); // for diffrent ports 
